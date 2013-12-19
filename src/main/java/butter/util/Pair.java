c3656ea54809279ae8b137f6da65343d0bb52110
lizhongyuan <lizhyvip@gmail.com>
2013-12-19 18:55:52 +0800

    pair对象只能用提供的工厂函数构造

--- src/main/java/butter/util/Pair.java
+++ src/main/java/butter/util/Pair.java
@@ -4,7 +4,7 @@ public class Pair<K, V> {
     private final K key;
     private final V value;
 
-    public Pair(K key, V value) {
+    private Pair(K key, V value) {
         this.key = key;
         this.value = value;
     }
